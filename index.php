818971f5347f3f94c07ab5dec4d7bc6c141534f7
Michael Onishchenko <onishchenko.michael@student.greenriver.edu>
2023-05-07 02:49:40 -0400

    added index.php with php to run post/get array

--- index.php
+++ index.php
@@ -1,85 +1,17 @@
 <?php
-/*
-    Michael Onishchenko
-    4/19/23
-    328/application/index.php
-    controller for application project
-
- */
 session_start();
 
 // Turn on error reporting
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
-
 require_once "vendor/autoload.php";
-
 const STATES = array(
-    "AL" => "Alabama",
-    "AK" => "Alaska",
-    "AZ" => "Arizona",
-    "AR" => "Arkansas",
-    "CA" => "California",
-    "CO" => "Colorado",
-    "CT" => "Connecticut",
-    "DE" => "Delaware",
-    "FL" => "Florida",
-    "GA" => "Georgia",
-    "HI" => "Hawaii",
-    "ID" => "Idaho",
-    "IL" => "Illinois",
-    "IN" => "Indiana",
-    "IA" => "Iowa",
-    "KS" => "Kansas",
-    "KY" => "Kentucky",
-    "LA" => "Louisiana",
-    "ME" => "Maine",
-    "MD" => "Maryland",
-    "MA" => "Massachusetts",
-    "MI" => "Michigan",
-    "MN" => "Minnesota",
-    "MS" => "Mississippi",
-    "MO" => "Missouri",
-    "MT" => "Montana",
-    "NE" => "Nebraska",
-    "NV" => "Nevada",
-    "NH" => "New Hampshire",
-    "NJ" => "New Jersey",
-    "NM" => "New Mexico",
-    "NY" => "New York",
-    "NC" => "North Carolina",
-    "ND" => "North Dakota",
-    "OH" => "Ohio",
-    "OK" => "Oklahoma",
-    "OR" => "Oregon",
-    "PA" => "Pennsylvania",
-    "RI" => "Rhode Island",
-    "SC" => "South Carolina",
-    "SD" => "South Dakota",
-    "TN" => "Tennessee",
-    "TX" => "Texas",
-    "UT" => "Utah",
-    "VT" => "Vermont",
-    "VA" => "Virginia",
-    "WA" => "Washington",
-    "WV" => "West Virginia",
-    "WI" => "Wisconsin",
-    "WY" => "Wyoming"
+    "AL" => "Alabama", "AK" => "Alaska", "AZ" => "Arizona", "AR" => "Arkansas", "CA" => "California", "CO" => "Colorado", "CT" => "Connecticut", "DE" => "Delaware", "FL" => "Florida", "GA" => "Georgia", "HI" => "Hawaii", "ID" => "Idaho", "IL" => "Illinois", "IN" => "Indiana", "IA" => "Iowa", "KS" => "Kansas", "KY" => "Kentucky", "LA" => "Louisiana", "ME" => "Maine", "MD" => "Maryland", "MI" => "Michigan", "MN" => "Minnesota", "MS" => "Mississippi", "MO" => "Missouri", "MT" => "Montana", "NE" => "Nebraska", "NV" => "Nevada", "NH" => "New Hampshire", "NJ" => "New Jersey", "NM" => "New Mexico", "NY" => "New York", "NC" => "North Carolina", "ND" => "North Dakota", "OH" => "Ohio", "OK" => "Oklahoma", "OR" => "Oregon", "PA" => "Pennsylvania", "RI" => "Rhode Island",
+    "SC" => "South Carolina", "SD" => "South Dakota", "TN" => "Tennessee", "TX" => "Texas", "UT" => "Utah", "VT" => "Vermont", "VA" => "Virginia", "WA" => "Washington", "WV" => "West Virginia", "WI" => "Wisconsin", "WY" => "Wyoming"
 );
-
-const MAILING_LIST_LANGUAGES = array(
-    "JavaScript", "PHP", "Java", "Python", "HTML", "CSS", "C", "C++"
-);
-const MAILING_LIST_VERTICALS = array(
-    "SaaS", "Healthcare", "Ag tech", "HR tech", "Industrial tech", "Cybersecurity"
-);
-
-const FORM_STATES = array(
-    "info" => "views/form_body_info.html",
-    "experience" => "views/form_body_experience.html",
-    "mailingList" => "views/form_body_mailing_lists.html",
-    "summary" => "views/form_body_summary.html"
-);
+const LANAGUAGES = array("JavaScript", "PHP", "Java", "Python", "HTML", "CSS", "ReactJS", "NodeJs");
+const VERTICALS = array("SaaS", "Health tech", "Ag-tech", "HR-tech", "Industrial tech", "Cybersecurity");
+const FORM_STATES = array("info" => "views/personal.html", "experience" => "views/exper.html", "mailing" => "views/mailing.html", "summary" => "views/summary.html");
 
 // Create an F3 (Fat-Free Framework) object
 $f3 = Base::instance();
@@ -90,62 +22,42 @@ $f3->route('GET /', function () {
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET|POST /apply', function ($f3) {
-    if (!isset($_SESSION["formState"]) || !array_key_exists($_SESSION["formState"], FORM_STATES)) {
-        // Beginning a new session
+$f3->route('GET|POST /personal', function ($f3) {
+    if (!array_key_exists("formState", $_SESSION) || !array_key_exists($_SESSION["formState"], FORM_STATES)) {
         $_SESSION = array();
         $_SESSION["formState"] = "info";
     } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Continuing an existing session
         $prevFormState = $_SESSION["formState"];
 
         // Move form data to session and update the form state
-        switch ($prevFormState) {
-            case "info":
-                $_SESSION["fname"] = $_POST["fname"];
-                $_SESSION["lname"] = $_POST["lname"];
-                $_SESSION["email"] = $_POST["email"];
-                $_SESSION["state"] = $_POST["state"];
-                $_SESSION["phone"] = $_POST["phone"];
-
-                $_SESSION["formState"] = "experience";
-                break;
-            case "experience":
-                $_SESSION["bio"] = $_POST["bio"];
-                $_SESSION["github"] = $_POST["github"];
-                $_SESSION["years"] = $_POST["years"];
-                $_SESSION["relocate"] = $_POST["relocate"];
-
-                $_SESSION["formState"] = "mailingList";
-                break;
-            case "mailingList":
-                if (isset($_POST["mailinglist"]))
-                    $_SESSION["mailinglist"] = $_POST["mailinglist"];
-
-                $_SESSION["formState"] = "summary";
-                break;
-            case "summary":
-                // For now, reset the session and reroute to home when the user clicks submit on the final summary page
-                $_SESSION = array();
-                $f3->reroute("/");
-                break;
+        if ($prevFormState == "info") {
+            $_SESSION["fname"] = isset($_POST["fname"]) ?$_POST["fname"] : null;
+            $_SESSION["lname"] = isset($_POST["lname"]) ? $_POST["lname"] : null;
+            $_SESSION["email"] = isset($_POST["email"]) ? $_POST["email"] : null;
+            $_SESSION["state"] = isset($_POST["state"]) ?$_POST["state"] : null;
+            $_SESSION["phone"] = isset($_POST["phone"])? $_POST["phone"] : null;
+            $_SESSION["formState"] = "experience";
+        } else if ($prevFormState == "experience") {
+            $_SESSION["bio"] = isset($_POST["bio"]) ? $_POST["bio"] : null;
+            $_SESSION["github"] = isset($_POST["github"]) ? $_POST["github"] : null;
+            $_SESSION["years"] = isset($_POST["years"]) ? $_POST["years"] : null;
+            $_SESSION["relocate"] = isset($_POST["relocate"]) ? $_POST["relocate"] : null;
+            $_SESSION["formState"] = "mailing";
+        } else if ($prevFormState == "mailing") {
+            $_SESSION["mailing"] = isset($_POST["mailing"]) ? $_POST["mailing"] : null;
+            $_SESSION["formState"] = "summary";
+        } else if ($prevFormState == "summary") {
+            $_SESSION = array();
+            $f3->reroute(".");
         }
     }
-
-    // Render the form page according to the current form state
-    $f3->set("formBody", FORM_STATES[$_SESSION["formState"]]);
-
+    $f3->set("body", FORM_STATES[$_SESSION["formState"]]);
     $f3->set("states", STATES);
-
-    $f3->set("languages", MAILING_LIST_LANGUAGES);
-    $f3->set("verticals", MAILING_LIST_VERTICALS);
+    $f3->set("languages", LANAGUAGES);
+    $f3->set("verticals", VERTICALS);
 
     $view = new Template();
-    echo $view->render('views/form_base.html');
+    echo $view->render('views/form.html');
 });
-
 // Run Fat-Free
 $f3->run();
-
-
-
