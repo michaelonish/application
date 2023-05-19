c5f17478957f5311bb5fb87505a6e033e78b416a
Michael Onishchenko <onishchenko.michael@student.greenriver.edu>
2023-05-19 18:08:09 -0400

    a few more validations and experimenting with data checking

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 session_start();
 
 // Turn on error reporting
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
+ini_set("display_errors", 0);
+error_reporting();
+require_once "model/validate.php";
+
 require_once "vendor/autoload.php";
 const STATES = array(
     "AL" => "Alabama", "AK" => "Alaska", "AZ" => "Arizona", "AR" => "Arkansas", "CA" => "California", "CO" => "Colorado", "CT" => "Connecticut", "DE" => "Delaware", "FL" => "Florida", "GA" => "Georgia", "HI" => "Hawaii", "ID" => "Idaho", "IL" => "Illinois", "IN" => "Indiana", "IA" => "Iowa", "KS" => "Kansas", "KY" => "Kentucky", "LA" => "Louisiana", "ME" => "Maine", "MD" => "Maryland", "MI" => "Michigan", "MN" => "Minnesota", "MS" => "Mississippi", "MO" => "Missouri", "MT" => "Montana", "NE" => "Nebraska", "NV" => "Nevada", "NH" => "New Hampshire", "NJ" => "New Jersey", "NM" => "New Mexico", "NY" => "New York", "NC" => "North Carolina", "ND" => "North Dakota", "OH" => "Ohio", "OK" => "Oklahoma", "OR" => "Oregon", "PA" => "Pennsylvania", "RI" => "Rhode Island",
@@ -51,13 +53,55 @@ $f3->route('GET|POST /personal', function ($f3) {
             $f3->reroute(".");
         }
     }
+
+
     $f3->set("body", FORM_STATES[$_SESSION["formState"]]);
     $f3->set("states", STATES);
     $f3->set("languages", LANAGUAGES);
     $f3->set("verticals", VERTICALS);
+
 
     $view = new Template();
     echo $view->render('views/form.html');
 });
 // Run Fat-Free
 $f3->run();
+
+
+
+
+
+    $validJobs = LANAGUAGES;
+    $validVerticals = VERTICALS;
+
+    $firstName = $_POST['fname'];
+    $lastName = $_POST['lname'];
+    $email = $_POST['email'];
+    $url = $_POST['github'];
+    $phone = $_POST['phone'];
+    $experience = $_POST['experience'];
+    $selectedJobs = $_POST['languages'];
+    $selectedVerticals = $_POST['verticals'];
+
+    if (validName($firstName) &&
+        validName($lastName) &&
+        validEmail($email) &&
+        validPhone($phone) &&
+        validExperience($experience) &&
+        validSelectionsJobs($selectedJobs, $validJobs) &&
+        validSelectionsVerticals($selectedVerticals, $validVerticals)) {
+        // All fields are valid, perform further actions
+
+    } else {
+        // Invalid fields, handle the validation errors
+        // ...
+    }
+
+
+
+
+
+
+
+
+
--- model/validate.php
+++ model/validate.php
@@ -19,4 +19,32 @@ function validExperience($value)
     return filter_var($value, FILTER_VALIDATE_INT, array('options' => array('min_range' => 0))) !== false;
 }
 
+function validPhone($phone)
+{
+    // Check if the phone number contains only numeric values
+    return ctype_digit($phone);
+}
+
+function validEmail($email)
+{
+    // Check if the email address is valid
+    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+}
+
+function validSelectionsJobs($selectedJobs, $validOptions)
+{
+    // Check each selected jobs checkbox selection against a list of valid options
+    // Assuming $selectedJobs is an array of selected job values
+    return empty(array_diff($selectedJobs, $validOptions));
+}
+
+function validSelectionsVerticals($selectedVerticals, $validOptions)
+{
+    // Check each selected verticals checkbox selection against a list of valid options
+    // Assuming $selectedVerticals is an array of selected vertical values
+    return empty(array_diff($selectedVerticals, $validOptions));
+}
+
+
+
 ?>
