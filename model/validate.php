9944853d3960f65216e67afa92fd61328711a25b
Michael Onishchenko <onishchenko.michael@student.greenriver.edu>
2023-05-19 16:28:21 -0400

    added validate.php and started validating functions

--- model/validate.php
+++ model/validate.php
@@ -12,6 +12,11 @@ function validGithub($url)
     return filter_var($url, FILTER_VALIDATE_URL) !== false;
 }
 
-
+function validExperience($value)
+{
+    // Check if the string is a valid "value" property (you can define the validation rules)
+    // For example, check if the value is a positive integer
+    return filter_var($value, FILTER_VALIDATE_INT, array('options' => array('min_range' => 0))) !== false;
+}
 
 ?>
